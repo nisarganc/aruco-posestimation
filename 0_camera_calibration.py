a39b53a2e922694b6d880005ec0f53c9abac2069
Nisarga Nilavadi <nisarganc7@gmail.com>
2024-11-06 21:13:41 +0100

    Update 0_camera_calibration.py

--- 0_camera_calibration.py
+++ 0_camera_calibration.py
@@ -5,7 +5,7 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((12*8,3), np.float32)
 objp[:,:2] = np.mgrid[0:12,0:8].T.reshape(-1,2)
-objp *= 31
+objp *= 31 # 31mm squares
 
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane
