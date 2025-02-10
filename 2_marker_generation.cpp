1692e8e8c66c79a1eac4d1f4c7fdd16bd183eec2
nisarganc <nisarganc7@gmail.com>
2025-02-10 10:32:28 +0100

    added other aruco prints

--- 2_marker_generation.cpp
+++ 2_marker_generation.cpp
@@ -7,7 +7,7 @@ int main()
         cv::Mat markerImage;
         cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_50);
         // pix = (16cm * 300dpi)/2.54 = 1889.8
-        cv::aruco::drawMarker(dictionary, 49, 1889.8, markerImage, 1);
-        cv::imwrite("marker49.png", markerImage);
+        cv::aruco::drawMarker(dictionary, 47, 1889.8, markerImage, 1);
+        cv::imwrite("marker47.png", markerImage);
         return 0;
     }
